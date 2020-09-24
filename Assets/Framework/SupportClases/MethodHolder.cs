e12c9af2e2ea6f6b23d1ce4cb7589b98f6649bad
Red <69144172+RedForest347@github.com>
2020-09-24 16:20:36 +0300

    MethodHolder патч первого часа

--- Assets/Framework/SupportClases/MethodHolder.cs
+++ Assets/Framework/SupportClases/MethodHolder.cs
@@ -22,22 +22,20 @@ namespace RangerV
 
         public void StartMethod()
         {
-            if (cashedMethodInfo == null)
-                Init();
-
-            cashedMethodInfo.Invoke(component, null);
+            BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+            Assembly.Load(assembly_name).GetType(type_name).GetMethod(method_name, bindingFlags).Invoke(component, null);
         }
 
 
         //используется для кеширования cashedMethodInfo и более быстрого последующего вызова
         // рекомендуется вызывать в старте или около того (при загрузке сцены)
-        public void Init()
+        /*public void Init()
         {
             if (assembly_name == "" || assembly_name == null)
                 throw new System.ArgumentNullException("не определена функция в MethodHolder, но произведена попытка ее вызова");
 
             cashedMethodInfo = Assembly.Load(assembly_name).GetType(type_name).GetMethod(method_name);
-        }
+        }*/
 
     }
 
---
+++ Assets/Framework/To tests/TestInspector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(Test))]
+public class TestInspector : Editor
+{
+    Test obj;
+
+    private void OnEnable()
+    {
+        obj = (Test)target;
+    }
+}
